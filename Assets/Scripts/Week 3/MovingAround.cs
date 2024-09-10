0d224a7256e0249ed478fda98aa23f887d960d66
Kyler V <kylergher@lcad.edu>
2024-09-10 11:39:03 -0700

    Physics and JUnk

--- Assets/Scripts/Week 3/MovingAround.cs
+++ Assets/Scripts/Week 3/MovingAround.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class MovingAround : MonoBehaviour
 {
+    public GameObject spawnedPrefab;
+    public Transform otherSpawnLocation;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        GameObject jeff = Instantiate(spawnedPrefab);
+
+        jeff.GetComponent<Light>().color = Color.blue;
+
+        Instantiate(spawnedPrefab, this.transform.position, this.transform.rotation);
+       // Instantiate(spawnedPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject jeffAgain = Instantiate (spawnedPrefab);
+            jeffAgain.GetComponent<Light>().color = Color.red;
+        }
+
+        if(Input.GetKeyUp(KeyCode.P))
+        {
+            GameObject jeffthree = Instantiate(spawnedPrefab, this.transform.position, this.transform.rotation);
+            jeffthree.GetComponent<Light>().color = Color.green;
+        }
+        
         
     }
+    public void CreateJeff()
+
+        {
+
+        }
 }
