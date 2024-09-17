5ae66f858bf893141eae45ac03cea0482d18cdd3
Kyler V <kylergher@lcad.edu>
2024-09-17 11:45:53 -0700

    ewihgckh
    wgeilG

---
+++ Assets/Scripts/Week 4/NewCannon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCannon : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public GameObject projectileSpawnPosition;
+    public float speed = 750f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireCannon()
+    {
+        GameObject Go = 
+    }
+}
