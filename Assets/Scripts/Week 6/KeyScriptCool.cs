3fd11e968a20945334d4c4f316749d525e9c9845
Kyler V <kylergher@lcad.edu>
2024-10-05 20:13:33 -0700

    uh oh i forgto

---
+++ Assets/Scripts/Week 6/KeyScriptCool.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum KeyColor
+{
+    Bronze, 
+    Silver, 
+    Gold,
+}
+
+public class KeyScriptCool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter()
+    {
+        
+    }
+}
